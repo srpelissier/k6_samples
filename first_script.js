8677cb618127eff7703645130ccc5cf9a1bc8e73
spelissi <sylvain.pelissier@capgemini.com>
2017-05-12 13:49:44 +0200

    add checks to first script

--- first_script.js
+++ first_script.js
@@ -1,5 +1,15 @@
 import http from "k6/http";
+import { check } from "k6";
+
+export let options = {
+    vus: 10,
+    duration: "30s";
+};
 
 export default function() {
-    http.get("http://asktheexpert.info");
+    let res = http.get("http://asktheexpert.info");
+    check(res, {
+        "status was 200": (r) => r.status == 200,
+        "transaction time OK": (r) => r.timings.duration < 200
+    });
 };
